8e975b5fc43dc22c83c1caf4f106c2b54d1e8e81
Ayush-Chauhan-7 <ayushmchauhan@gmail.com>
2024-06-07 17:36:52 +0530

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
    {
       title: 'Movie Recommendation Using Vectorization',
-      imgUrl: 'public/assets/recommender.png',
+      imgUrl: 'https://i.imgur.com/1zQskel.png/assets/gamingconsole.png',
       stack: ['SKLearn', 'Python', 'StreamLit'],
       link: 'https://movie-suggestor.streamlit.app/',
    },
    {
       title: 'Game Console for Disabled Kids',
-      imgUrl: 'public/assets/gamingconsole.png',
+      imgUrl: 'https://i.imgur.com/ECNoZtJ.png',
       stack: ['ESP-IDF', 'C#', 'Unix Terminal'],
       link: 'https://diodedarlings.my.canva.site/',
    },
    {
       title: 'This Website',
-      imgUrl: 'public/assets/website.png',
+      imgUrl: 'https://i.imgur.com/482vCmL.png',
       stack: ['Vite', 'React JS', 'Tailwind CSS'],
       link: '',
    },
