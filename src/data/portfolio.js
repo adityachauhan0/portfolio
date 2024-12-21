c6b68eb20e4b499a30b86b895943f5ecfeda50f0
adityachauhan0 <127509777+adityachauhan0@users.noreply.github.com>
2024-12-21 19:50:33 +0530

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,5 +1,17 @@
 export default [
    {
+      title: '3D portfolio',
+      imgUrl: 'https://i.imgur.com/UuPtfXz.png',
+      stack: ['NextJS', 'React', 'ThreeJS', 'TailWind CSS', 'Framer Motion', 'EmailJS'],
+      link: 'https://portfol-adityac.netlify.app/',
+   },
+   {
+      title: 'Sonic Endless Runner',
+      imgUrl: 'https://i.imgur.com/kHlRo73.png',
+      stack: ['JavaScript', 'Kaplay JS', 'Vite', 'TailWind CSS'],
+      link: 'https://sonic-js.netlify.app/',
+   },
+   {
       title: 'Movie Recommendation Using Vectorization',
       imgUrl: 'https://i.imgur.com/1zQskel.png/assets/gamingconsole.png',
       stack: ['SKLearn', 'Python', 'StreamLit'],
